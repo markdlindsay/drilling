67ab0927b6a490891494a75730ccf2f825b51560
Mark Lindsay <markdlindsay@gmail.com>
2021-03-24 16:20:21 +0800

    Add data-raw to local repo

---
+++ data-raw/data_prep.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+FYH_drill <- read.csv("./data-raw/FiveYamarnaHoles_withID.csv", stringsAsFactors = TRUE)
+FYH_drill <- FYH_drill[-1,] # remove 'helper' first row that makes factoring easier.
+levels(FYH_drill[,1]) <- factor(FYH_drill[,1]) # re-factor the drill hole name column to remove un-needed labels
+colnames(FYH_drill)[45] <- "Fe_perc" # rename geochem column to include units
+usethis::use_data(FYH_drill, overwrite = TRUE)
+
+
